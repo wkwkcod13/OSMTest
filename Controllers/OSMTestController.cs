3d0f0b9d084cf61b37cc0e882264ac2c0541b5fa
ken <ken@DESKTOP-UTL70AJ>
2025-06-05 00:03:15 +0800

    加入專案檔案。

---
+++ Controllers/OSMTestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OSMTest.Controllers
+{
+    [Route("[controller]")]
+    public class OSMTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
